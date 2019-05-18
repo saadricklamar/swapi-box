c029752d928ed90f66b495bf98ed2d5a8c50d708
Saad Baradan <saadbaradan@gmail.com>
2019-05-18 13:24:27 -0600

    Add hover functionality to all cards

--- src/People/People.js
+++ src/People/People.js
@@ -4,12 +4,15 @@ import './People.css';
 const People = (props) => {
   const cardInfo = props.people.map(card => {
     let personName = card.name;
-    return <article className='people'>
+    return <article className='card'>
                 <img src={require(`../images/${personName}.jpeg`)}/>
-                <h2 className='name'>{card.name}</h2>
-                <h2>Species: {card.species}</h2>
-                <h2>Homeworld: {card.homeworld}</h2>
-                <h2>Population: {card.population}</h2>
+                <i className='fas fa-star'></i> 
+                <div className='dark-background'>
+                  <h2 className='name'>{card.name}</h2>
+                  <h2>Species: {card.species}</h2>
+                  <h2>Homeworld: {card.homeworld}</h2>
+                  <h2>Population: {card.population}</h2>
+                </div>
             </article>
   });
     return (
