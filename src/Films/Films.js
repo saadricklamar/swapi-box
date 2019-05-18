b954733f601691addafd0e65bebd0859a24022cc
Saad Baradan <saadbaradan@gmail.com>
2019-05-18 15:08:57 -0600

    Clean film data by adding key

--- src/Films/Films.js
+++ src/Films/Films.js
@@ -3,19 +3,28 @@ import './Films.css';
 
 
 const Films = (props) => {
-  const cardInfo = props.films.map(card => {
+  const cleanCard = props.films.map(card => {
+    return ({
+      title: card.title,
+      episode: card.episode_id,
+      released: card.release_date,
+      director: card.director,
+      id: card.episode_id
+    })})
+    
+   const cardInfo = cleanCard.map(card => {
     let filmName = card.title;
-      return  <article className='card'>
-                <img src={require(`../images/${filmName}.jpeg`)}/>
-                <i className='fas fa-star'></i> 
-                <div className='dark-background'>
-                  <h2 className='name'>{card.title}</h2>
-                  <h2>Episode: {card.episode_id}</h2>
-                  <h2>Released: {card.release_date}</h2>
-                  <h2>Director: {card.director}</h2>
-                </div>
-            </article>
-  })
+    return  <article className='card' key={card.id}>
+              <img src={require(`../images/${filmName}.jpeg`)}/>
+              <i className='fas fa-star'></i> 
+              <div className='dark-background'>
+                <h2 className='name'>{card.title}</h2>
+                <h2>Episode: {card.episode}</h2>
+                <h2>Released: {card.released}</h2>
+                <h2>Director: {card.director}</h2>
+              </div>
+          </article>
+   })
     return (
     <div className='cardsContainer'>
      {cardInfo}
