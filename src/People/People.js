839e7eeeb792d529b43446a477b2cde7bf031171
Saad Baradan <saadbaradan@gmail.com>
2019-05-19 16:27:12 -0600

    Add favoriting functionality for people, planets, and vehicles

--- src/People/People.js
+++ src/People/People.js
@@ -2,13 +2,21 @@ import React from 'react';
 import './People.css';
 
 const People = (props) => {
-  const cardInfo = props.people.map(card => {
-    let personName = card.name;
-    return <article className='card'>
+  const cleanCard = props.people.map(card => {
+    return ({
+      title: card.name,
+      species: card.species,
+      homeworld: card.homeworld,
+      population: card.population,
+      id: card.height
+    })})
+  const cardInfo = cleanCard.map(card => {
+    let personName = card.title;
+    return <article className='card' key={card.id}>
                 <img src={require(`../images/${personName}.jpeg`)}/>
-                <i className='fas fa-star'></i> 
+                <i className='fas fa-star' onClick={() => props.toggleFavorite(card)}></i> 
                 <div className='dark-background'>
-                  <h2 className='name'>{card.name}</h2>
+                  <h2 className='name'>{card.title}</h2>
                   <h2>Species: {card.species}</h2>
                   <h2>Homeworld: {card.homeworld}</h2>
                   <h2>Population: {card.population}</h2>
--- src/Vehicles/Vehicles.js
+++ src/Vehicles/Vehicles.js
@@ -3,15 +3,24 @@ import './Vehicles.css';
 
 
 const Vehicles = (props) => {
-  const cardInfo = props.vehicles.map(card => {
-    let vehicleName = card.name;
-      return  <article className='card'>
+  const cleanCard = props.vehicles.map(card => {
+    return ({
+      title: card.name,
+      model: card.model,
+      class: card.vehicle_class,
+      passengers: card.passengers,
+      id: card.length
+    })})
+
+  const cardInfo = cleanCard.map(card => {
+    let vehicleName = card.title;
+      return  <article className='card' key={card.id}>
                 <img src={require(`../images/${vehicleName}.jpeg`)}/>
-                <i className='fas fa-star'></i> 
+                <i className='fas fa-star' onClick={() => props.toggleFavorite(card)}></i> 
                 <div className='dark-background'>
-                  <h2 className='name'>{card.name}</h2>
+                  <h2 className='name'>{card.title}</h2>
                   <h2>Model: {card.model}</h2>
-                  <h2>Class: {card.vehicle_class}</h2>
+                  <h2>Class: {card.class}</h2>
                   <h2>Passengers: {card.passengers}</h2>
                 </div>
             </article>
